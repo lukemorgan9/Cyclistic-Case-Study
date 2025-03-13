8bb66313e587d7e77be2166db295166b969d0d68
lukemorgan9 <l.k.n.morgan9@gmail.com>
2025-03-13 12:41:01 +0000

    Update data_exploration.sql

--- data_exploration.sql
+++ data_exploration.sql
@@ -1,1 +1,12 @@
+/*
+#1 Veryifing that all values in ride_id column are unique
+*/
+
+SELECT COUNT(ride_id) - COUNT(DISTINCT ride_id) AS duplicate_rows
+FROM `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata` 
+
+/*
+NOTES: There are 211 duplicate rows which will need to be removed during cleaning
+
+
 
