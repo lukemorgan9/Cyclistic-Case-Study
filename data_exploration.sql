e99d21151213060ccd96b8c6890825d9689ea7e0
lukemorgan9 <l.k.n.morgan9@gmail.com>
2025-03-16 20:30:11 +0000

    Update data_exploration.sql

--- data_exploration.sql
+++ data_exploration.sql
@@ -1,7 +1,7 @@
 -- #1 Veryifing that all values in ride_id column are unique
 */
 SELECT COUNT(ride_id) - COUNT(DISTINCT ride_id) AS duplicate_rows
-FROM `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata` 
+FROM `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata`; 
 /*
 NOTES: There are 211 duplicate rows which will need to be removed during cleaning.
 
@@ -29,7 +29,7 @@ WHERE TIMESTAMP_DIFF(ended_at, started_at, MINUTE) <= 1 OR
 /*
 NOTES: There are 250270 such trips which will need to be removed during cleaning.
 
--- #5 Checking start_station_name/start_station_id and end_station_name/end_station_id columns for NULL values and inconsisten
+-- #5 Checking start_station_name/start_station_id and end_station_name/end_station_id columns for NULL values and inconsistencies
 */
 SELECT start_station_name, COUNT(*) AS occurences
 FROM `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata`
@@ -38,8 +38,8 @@ ORDER BY occurences DESC;
 -- NULL is most common with 1073951 occurences
 
 SELECT COUNT(DISTINCT(start_station_name)) AS unique_start_name,          -- 1808 unique start station names
-   COUNT(DISTINCT(start_station_id)) AS unique_start_id,                  -- 1763 unique start station ids
-FROM `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata`
+       COUNT(DISTINCT(start_station_id)) AS unique_start_id,              -- 1763 unique start station ids
+FROM `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata`;
 
 SELECT end_station_name, COUNT(*) AS occurences
 FROM `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata`
@@ -47,6 +47,30 @@ GROUP BY end_station_name
 ORDER BY occurences DESC;
 -- NULL is most common with 1104653 occurences
 
+SELECT COUNT(DISTINCT(end_station_name)) AS unique_end_name,
+       COUNT(DISTINCT(end_station_id)) AS unique_end_id,                   -- 1815 unique end station names
+FROM `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata`;   -- 1768 unique end station ids
+/*
+NOTES: Due to the high number of nulls, errors and inconsistencies, I will ignore the station names and ids and use lat and long instead.
+
+-- #6 Checking for null values in start and end latitude and longitude columns
+*/ 
+SELECT *
+FROM `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata`
+WHERE start_lat IS NULL OR
+      start_lng IS NULL OR
+      end_lat IS NULL OR
+      end_lng IS NULL;
+/*
+NOTES: There are 7232 of these rows which will need to be removed during cleaning.
+
+-#7 Verifying that there are only 2 member types
+*/ 
+SELECT DISTINCT member_casual, COUNT(member_casual) AS trips
+FROM `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata`
+GROUP BY member_casual;
+/*
+NOTES: 'member' and 'casual' are the only 2 values that occur in this column.
 
 
 
