8eed054eb81f62bde70953bfc7c56246130ba45d
lukemorgan9 <l.k.n.morgan9@gmail.com>
2025-03-20 13:08:37 +0000

    Update data_cleaning.sql

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -1,1 +1,9 @@
-
+-- #1 Removing duplicate ride ids
+*/
+DELETE FROM `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata`
+WHERE ended_at NOT IN
+(
+SELECT MAX(ended_at)
+        FROM `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata`
+        GROUP BY [ride_id]
+    );
