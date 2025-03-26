e99c3bf0ece64198b0e42c36566cbb9c56f83c7e
lukemorgan9 <l.k.n.morgan9@gmail.com>
2025-03-26 14:38:29 +0000

    Update data_cleaning.sql

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -54,4 +54,17 @@ TIMESTAMP_DIFF(ended_at, started_at, MINUTE) AS ride_length_minutes,
 FROM `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata`
 /*
 __________________________________________________________________________________________________
+-- #6 Changing null values in start and end station names to 'bike_lock'
+*/
+UPDATE `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata` 
+SET start_station_name = 'bike_lock'
+WHERE start_station_name IS NULL
+
+UPDATE `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata` 
+SET end_station_name = 'bike_lock'
+WHERE end_station_name IS NULL
+/*
+NOTES: 1,959,684 null values successfully modified.
+__________________________________________________________________________________________________
+
 NOTES: 250,479 rows successfully removed. 5,610,089 rows of clean data remain.
