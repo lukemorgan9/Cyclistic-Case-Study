b416467e023c76e8c34b7b834405183091c97d2e
lukemorgan9 <l.k.n.morgan9@gmail.com>
2025-03-27 12:29:04 +0000

    Update data_analysis.sql

--- data_analysis.sql
+++ data_analysis.sql
@@ -1,1 +1,13 @@
+-- #1 Popularity of different rideable types
+*/
+CREATE TABLE cyclistic_tripdata.rideable_type_popularity AS
+SELECT rideable_type, member_casual, COUNT(*) AS number_of_trips
+   FROM `cyclistic-case-study-452522.cyclistic_tripdata.combined_tripdata`
+   GROUP BY rideable_type, member_casual
+   ORDER BY member_casual, number_of_trips DESC;
+/*
+__________________________________________________________________________________________________
+-- #2 Number of trips per month
+*/
+
 
