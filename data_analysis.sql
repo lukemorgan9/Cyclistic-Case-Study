c127595ad9bc3cac225c62b204d0747c6a7a1d06
lukemorgan9 <l.k.n.morgan9@gmail.com>
2025-04-02 12:41:09 +0100

    Update data_analysis.sql

--- data_analysis.sql
+++ data_analysis.sql
@@ -31,11 +31,54 @@ SELECT member_casual, EXTRACT(HOUR FROM started_at) AS time_of_day, COUNT(*) AS 
    GROUP BY member_casual, time_of_day;
 /*
 __________________________________________________________________________________________________
--- #5 Average ride length per month
+-- #5 Average trip duration per month
 */
 CREATE TABLE cyclistic_tripdata.avg_duration_monthly AS
 SELECT member_casual, month, ROUND(AVG(ride_length_minutes), 0) AS avg_trip_duration
    FROM `cyclistic-case-study-452522.cyclistic_tripdata.date_formatted_tripdata`
    GROUP BY member_casual, month
    ORDER BY member_casual;
-
+/*
+__________________________________________________________________________________________________
+-- #6 Average trip duration per day of week
+*/
+CREATE TABLE cyclistic_tripdata.avg_duration_day AS
+SELECT member_casual, day_of_week, ROUND(AVG(ride_length_minutes), 0) AS avg_trip_duration
+   FROM `cyclistic-case-study-452522.cyclistic_tripdata.date_formatted_tripdata`
+   GROUP BY member_casual, day_of_week
+   ORDER BY member_casual;
+/*
+__________________________________________________________________________________________________
+-- #7 Average trip duration per hour of the day
+*/
+CREATE TABLE cyclistic_tripdata.avg_duration_hourly AS
+SELECT EXTRACT(HOUR FROM started_at) AS hour_of_day, member_casual, AVG(ride_length_minutes) AS avg_trip_duration
+   FROM `cyclistic-case-study-452522.cyclistic_tripdata.date_formatted_tripdata`
+   GROUP BY member_casual, hour_of_day
+   ORDER BY member_casual;
+/*
+__________________________________________________________________________________________________
+-- #8 Start station popularity
+*/
+CREATE TABLE cyclistic_tripdata.start_station_pop AS
+SELECT start_station_name, member_casual,
+   ROUND(AVG(start_lat), 4) AS start_lat,
+   ROUND(AVG(start_lng), 4) AS start_lng,  
+   COUNT(*) AS total_trips
+FROM `cyclistic-case-study-452522.cyclistic_tripdata.date_formatted_tripdata`
+WHERE start_station_name != 'bike_lock'
+   GROUP BY start_station_name, member_casual; 
+/*
+__________________________________________________________________________________________________
+-- #9 End station popularity
+*/
+CREATE TABLE cyclistic_tripdata.end_station_pop AS
+SELECT end_station_name, member_casual,
+   ROUND(AVG(end_lat), 4) AS end_lat,
+   ROUND(AVG(end_lng), 4) AS end_lng,  
+   COUNT(*) AS total_trips
+FROM `cyclistic-case-study-452522.cyclistic_tripdata.date_formatted_tripdata`
+WHERE end_station_name != 'bike_lock'
+   GROUP BY end_station_name, member_casual;
+/*
+__________________________________________________________________________________________________
